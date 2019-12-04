ca304366d752664da440b3ada6c71f0a12b451b4
Hanyu <hl3304@columbia.edu>
2019-12-04 22:09:54 +0000

    1204django

--- adopt/views.py
+++ adopt/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from .models import Pet
+from .forms import PetForm
 
 def all_pets(request):
     pets = Pet.objects.all()
@@ -12,3 +13,41 @@ def all_pets(request):
 def pet_details(request,pet_id):
     pet = Pet.objects.get(id=pet_id)
     return HttpResponse(f"Hi, i'm pet {pet.id}")
+
+def edit_pet(request,pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    if request.method == 'POST':
+        # check form data
+        form = PetForm(request.POST, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/adopt/{pet_id}')
+    else:
+        form = PetForm(instance=pet)
+
+    context = {
+            'form':form,
+            }
+    return render(request, 'adopt/edit.html',context)
+
+
+def add_pet(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/adopt/list')
+    else:
+        form = PetForm()
+
+    context ={
+            'form':form,
+            )
+    return render(request,'adopt/edit.html',context)
+
+
+
+
+
+
+
